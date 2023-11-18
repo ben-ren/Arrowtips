69f2fb6fdb59400a7a072ad21c5cf4bdb86c37d6
Benjamin Rendell <benjamin.rendell@students.mq.edu.au>
2023-11-18 14:33:32 +1100

    Added conditional styling to navbar. Installed React-Router.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,26 @@ export default function Navbar(){
     return(
         <nav className="nav">
             <ul>
-                <li className="active"><a href="/" className="navbar">Home</a></li>
-                <li><a href="/about" className="navbar">About</a></li>
-                <li><a href="/posts" className="navbar">Posts</a></li>
-                <li><a href="/games" className="navbar">Games</a></li>
-                <li><a href="/projects" className="navbar">Projects</a></li>
+                <CustomLink link="/" content="Home"></CustomLink>
+                <CustomLink link="/about" content="About"></CustomLink>
+                <CustomLink link="/posts" content="Posts"></CustomLink>
+                <CustomLink link="/games" content="Games"></CustomLink>
+                <CustomLink link="/projects" content="Projects"></CustomLink>
             </ul>
         </nav>
+    )
+}
+
+type CustomLinkProps = {
+    link: string;
+    content: string;
+}
+
+function CustomLink({link, content, ...props}: CustomLinkProps){
+    const path = window.location.pathname
+    return (
+        <li className={path === link ? "active" : ""}>
+            <a href={link} {...props} className="navbar">{content}</a>
+        </li>
     )
 }
